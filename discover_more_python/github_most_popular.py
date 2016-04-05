d769b9c08380371a052468b17ed670d50fb9d625
Siphan <carole.bou.ibms@gmail.com>
2016-04-05 08:21:39 +0000

    Correct task 1

--- discover_more_python/github_most_popular.py
+++ discover_more_python/github_most_popular.py
@@ -1,10 +1,19 @@
-import requests
+""" This program prints the response from Github's API when requesting 
+    the 30 most popular Python projects
+"""
+
+from urllib2 import Request, urlopen, URLError
 
 request_headers = {
-  'User-Agent': 'Holberton_School',
-  'Authorization': 'token 170f8634262556331b3cb1809ffd4eb6e6fd40e1'
-}
+            'User-Agent': 'Holberton School',
+            'Authorization': 'token 170f8634262556331b3cb1809ffd4eb6e6fd40e1'
+        }
 
-r = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', request_headers)
+request = Request('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers=request_headers)
 
-print r.content
+try:
+        response = urlopen(request)
+        string = response.read()
+        print string
+except URLError, e:
+    print "Error code: ", e
