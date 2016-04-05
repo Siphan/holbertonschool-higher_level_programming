7c28d99f9e50a9c68905c261c30d7e72ec4e3c4e
Carole Bou <35@holbertonschool.com>
2016-04-05 01:51:45 -0700

    Correct answers for resubmission

--- discover_more_python/manipulate_the_data.py
+++ discover_more_python/manipulate_the_data.py
@@ -1,14 +1,25 @@
-import requests
+""" This program parses the response from Github's API when requesting 
+    the 30 most popular Python projects to a file into a JSON format and
+    prints the name of each project
+"""
+
+from urllib2 import Request, urlopen, URLError
 import json
-# function to parse json and print specific field
+
 request_headers = {
-    'User-Agent': 'Holberton_School',
- 'Authorization': '170f8634262556331b3cb1809ffd4eb6e6fd40e1'
+    'User-Agent': 'Holberton School',
+    'Authorization': 'token 170f8634262556331b3cb1809ffd4eb6e6fd40e1'
 }
 
-r = requests.get('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', request_headers)
-c = r.content
-j = json.loads(c)
+request = Request('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers=request_headers)
 
-for k in j['items']:
-    print k['full_name']
+try:
+            response = urlopen(request)
+            string = response.read()
+            parsed = json.loads(string)
+
+            for item in parsed["items"]:
+                        print item.get("full_name")
+except URLError, e:
+        print "Error code: ", e
+        
