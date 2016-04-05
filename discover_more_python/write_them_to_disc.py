f383ba83ba0a37ce9ddaf97bb694f8be3e04b037
Siphan Bou <35@holbertonschool.com>
2016-04-05 12:13:15 -0700

    Correct indents

--- discover_more_python/write_them_to_disc.py
+++ discover_more_python/write_them_to_disc.py
@@ -3,9 +3,9 @@
 from urllib2 import Request, urlopen, URLError
 
 request_headers = {
-                'User-Agent': 'Holberton School',
-                'Authorization': 'token 170f8634262556331b3cb1809ffd4eb6e6fd40e1'
-           }
+            'User-Agent': 'Holberton School',
+            'Authorization': 'token 170f8634262556331b3cb1809ffd4eb6e6fd40e1'
+        }
 
 request = Request('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers=request_headers)
 
@@ -14,11 +14,11 @@ file = open(filename, 'w')
 file.truncate()
 
 try:
-            response = urlopen(request)
-            string = response.read()
-            file.write(string)
-            print "The file was saved!"
+        response = urlopen(request)
+        string = response.read()
+        file.write(string)
+        print "The file was saved!"
 except URLError, e:
-        print "Error code: ", e
+    print "Error code: ", e
 
-        file.close()
+file.close()
