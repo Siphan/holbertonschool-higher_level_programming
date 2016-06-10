d9426e62a41865d5966f84cbec31db77cbfebe34
Carole Bou <35@holbertonschool.com>
2016-06-10 16:45:07 -0700

    Test

---
+++ test/TechCompanies/TechCompanies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechCompanies
+//
+//  Created by Siphan Bou on 6/8/16.
+//  Copyright © 2016 Siphan Bou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
